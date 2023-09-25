8f4c4afcf158a4c5641b9ac0b27a3b6bd5f235c5
Oleksandr Chaban <vrmonterrey@MacBook-Air-Oleksandr.local>
2023-09-25 21:45:43 +0200

    added tags and many-to-many rl between tags and items

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
 from db import db
 
 
@@ -28,7 +29,8 @@ def create_app(db_url=None):
     with app.app_context():
         db.create_all()
 
-    api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(TagBlueprint)
 
     return app
--- schemas.py
+++ schemas.py
@@ -12,6 +12,11 @@ class PlainStoreSchema(Schema):
     name = fields.Str(required=True)
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
@@ -22,10 +27,29 @@ class StoreUpdateSchema(Schema):
     name = fields.Str()
 
 
+class TagUpdateSchema(Schema):
+    name = fields.Str()
+    store_id = fields.Int()
+
+
 class ItemSchema(PlainItemSchema):
-    store_id = fields.Str(required=True, load_only=True)
+    store_id = fields.Int(required=True, load_only=True)
     stores = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.Nested(PlainTagSchema(), many=True, dump_only=True)
 
 
 class StoreSchema(PlainStoreSchema):
-    items = fields.Nested(PlainItemSchema(), dump_only=True)
+    items = fields.Nested(PlainItemSchema(), many=True, dump_only=True)
+    tags = fields.Nested(PlainTagSchema(), many=True, dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(dump_only=True)
+    stores = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.Nested(PlainItemSchema(), many=True, dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
